fe3170dd86ac8292cb310d9dc70371b9a1b0099c
Your Name <you@example.com>
2023-03-30 08:06:06 -0500

    se crea calculadora

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def calcular (numero1,numero2,numero3,numero4,numero5):
+    suma=numero1+numero2+numero3+numero4+numero5
+    return (suma)
+    
+resultado=sumar(1,2,3,4,5)
+print(resultado)
+    
+    
