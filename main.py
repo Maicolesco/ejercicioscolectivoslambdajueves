e57ad31b13ba23f67c6eff07a474f19c150814fb
Nataliacardenas <ncardenasrestrepo@gmail.com>
2023-03-30 08:18:04 -0500

    Se inicio diccionario

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-nombres = ['Santiago', 'Ruddy', 'Daisy', 'Mafe', 'Pablo', 'Juan Jose', 'Carlos', 'Luis']
+personas = []
+datos = {
+    "nombre",
+    "id",
+    "tel"
+}
+def RegistrarDato():
+    for i in range(0, 2):
+        nombre = input("Digite el nombre: ")
+        id = int(input("Digite el id: "))
+        tel = int(input("Digite el telefono: "))
+        datos = {"nombre": nombre, "id": id, "tel": tel}
+        personas.append(datos)
 
 def MostrarNombres(nombres):
     for i in range(0, len(nombres)):
         print(nombres[i])
         
-MostrarNombres(nombres)
+RegistrarDato()
+print(personas)
